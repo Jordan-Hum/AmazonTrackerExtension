376201ca4b321e2c6c798bc0e6e59c55a58acec2
JordanHum <jhum268@gmail.com>
2020-08-18 12:36:15 -0400

    added check for empty input value

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ let changeColor = document.getElementById('changeColor');
 var i = 0;
 
 addItem.onclick = function() {
-    
+
     var URLValue = document.getElementById('itemURL').value;
 
     /*var remove = "<button type = 'button' class='removeButton'> Remove Item </button>";
@@ -18,36 +18,37 @@ addItem.onclick = function() {
 
         return false;
     }*/
+    if(URLValue.length > 0){
+        var div = document.createElement('div');
+        div.style.marginTop = "10px";
+        div.style.backgroundColor = "#c2e1ff";
+        div.style.borderLeft = "6px solid #2196F3";
 
-    var div = document.createElement('div');
-    div.style.marginTop = "10px";
-    div.style.backgroundColor = "#c2e1ff";
-    div.style.borderLeft = "6px solid #2196F3";
+        var par = document.createElement("P");
 
-    var par = document.createElement("P");
+        var name = document.createElement('name');
+        name.innerHTML = URLValue + "  ";
 
-    var name = document.createElement('name');
-    name.innerHTML = URLValue + "  ";
+        var priceWhenAdded = document.createElement('priceWhenAdded');
+        priceWhenAdded.innerHTML = "50.00  ";
 
-    var priceWhenAdded = document.createElement('priceWhenAdded');
-    priceWhenAdded.innerHTML = "50.00  ";
+        var priceCurrent = document.createElement('priceCurrent');
+        priceCurrent.innerHTML = "30.00";
 
-    var priceCurrent = document.createElement('priceCurrent');
-    priceCurrent.innerHTML = "30.00";
+        var favoriteItem = document.createElement("BUTTON");
+        favoriteItem.innerHTML = "Favorite Item";
 
-    var favoriteItem = document.createElement("BUTTON");
-    favoriteItem.innerHTML = "Favorite Item";
+        var removeItem = document.createElement("BUTTON");
+        removeItem.innerHTML = "Remove Item";
 
-    var removeItem = document.createElement("BUTTON");
-    removeItem.innerHTML = "Remove Item";
+        par.appendChild(name);
+        par.appendChild(priceWhenAdded);
+        par.appendChild(priceCurrent);
 
-    par.appendChild(name);
-    par.appendChild(priceWhenAdded);
-    par.appendChild(priceCurrent);
+        div.appendChild(par);
+        div.appendChild(favoriteItem);
+        div.appendChild(removeItem);
 
-    div.appendChild(par);
-    div.appendChild(favoriteItem);
-    div.appendChild(removeItem);
-
-    document.body.appendChild(div);
+        document.body.appendChild(div);
+    }
 };
