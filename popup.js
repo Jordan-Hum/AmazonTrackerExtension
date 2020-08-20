42a7ef87ec3637619f67af1f63f3f6f128d77b8d
Caleb Lim <caleb.lim@mail.mcgill.ca>
2020-08-20 12:47:07 -0400

    added dom functionality after closing extension

--- popup.js
+++ popup.js
@@ -1,13 +1,91 @@
 var i = 0;
+
+var tempNameArray = [];
+var tempPriceAddedArray = [];
+var tempPriceCurrrentArray = [];
+
 var nameArray = [];
 var priceAddedArray = [];
 var priceCurrentArray = [];
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    chrome.storage.local.get('names', function(result){
+        tempNameArray = result.names;
+        alert(tempNameArray.length);
+    })
+
+    chrome.storage.local.get('pricesAdded', function(result){
+        tempPriceAddedArray = result.pricesAdded;
+    })
+
+    chrome.storage.local.get('pricesCurrent', function(result){
+        tempPriceCurrentArray = result.pricesCurrent;
+    })
+
+    if(tempNameArray.length > 0){
+        alert('into if');
+        alert('getting '+tempNameArray[0]);
+        alert(tempNameArray.length);
+        for(j=0;j<tempNameArray.length;j++){
+            alert('into for');
+            if(tempNameArray[j]!=null){
+                addtoDOM(tempNameArray[j], tempPriceAddedArray[j], tempPriceCurrentArray[j]);
+            }
+        }
+    }
+});
+
+//read from storage and restore to popup
+function addtoDOM(iName, iPriceAdded, iPriceCurrent){
+    alert('calling addtodom');
+
+    var div = document.createElement('div');
+    div.style.marginTop = "10px";
+    div.style.backgroundColor = "#c2e1ff";
+    div.style.borderLeft = "6px solid #2196F3";
+
+    var par = document.createElement("P");
+
+    var name = document.createElement('name');
+    name.innerHTML = iName + "  ";
+
+    var priceWhenAdded = document.createElement('priceWhenAdded');
+    priceWhenAdded.innerHTML = iPriceAdded + "  ";
+
+    var priceCurrent = document.createElement('priceCurrent');
+    priceCurrent.innerHTML = iPriceCurrent;
+
+    var favoriteItem = document.createElement("BUTTON");
+    favoriteItem.innerHTML = "Favorite Item";
+
+    var removeItem = document.createElement("BUTTON");
+    removeItem.innerHTML = "Remove Item";
+
+    removeItem.onclick = function(){
+        div.remove();
+        chrome.storage.local.remove('names', function(){
+            alert('name removed from storage');
+        });
+    }
+
+    par.appendChild(name);
+    par.appendChild(priceWhenAdded);
+    par.appendChild(priceCurrent);
+
+    div.appendChild(par);
+    div.appendChild(favoriteItem);
+    div.appendChild(removeItem);
+
+    document.body.appendChild(div);
+}
 
 addItem.onclick = function() {
 
     var URLValue = document.getElementById('itemURL').value;
 
     if(URLValue.length > 0){
+
         var div = document.createElement('div');
         div.style.marginTop = "10px";
         div.style.backgroundColor = "#c2e1ff";
@@ -18,19 +96,6 @@ addItem.onclick = function() {
         var name = document.createElement('name');
         name.innerHTML = URLValue;
         nameArray[i] = name.innerHTML;
-
-        /*chrome.storage.sync.set({'name${i}':nameArray[i]}, function(){
-            chrome.storage.sync.get('name${i}', function(result){
-                alert(result.name${i});
-            });
-        }); */
-        /*chrome.storage.local.set({'nameArray':nameArray}, function(){
-            chrome.storage.local.get('nameArray', function(result){
-                for(k=0;k<10;k++){
-                    alert(result.nameArray[k]);
-                }
-            });
-        }); */
 
         var priceWhenAdded = document.createElement('priceWhenAdded');
         priceWhenAdded.innerHTML = "50.00";
@@ -63,8 +128,16 @@ addItem.onclick = function() {
 
         document.body.appendChild(div);
 
-        chrome.storage.local.set({'names': name.innerHTML} , function(){
-            alert('put into storage');
+        chrome.storage.local.set({'names': nameArray}, function(){
+            //console.log("put into storage");
+        })
+
+        chrome.storage.local.set({'pricesAdded': priceAddedArray} , function(){
+            //console.log("put into storage");
+        })
+
+        chrome.storage.local.set({'pricesCurrent': priceCurrentArray} , function(){
+            //console.log("put into storage");
         })
 
         for(j=0;;j++){
@@ -78,7 +151,10 @@ addItem.onclick = function() {
 
 storageTest.onclick = function() {
     chrome.storage.local.get('names', function(result) {
-        alert(result.names);
+        //alert(result.names);
+        for (k=0;k<result.names.length;k++){
+            console.log(result.names[k]);
+        }
     });
 }
 
